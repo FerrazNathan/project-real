3ce4405eb4cc31b01e779fabac1d9a6a75b04233
Nathan Ferraz <nathanferraz@hotmail.com>
2022-05-30 10:31:02 -0300

    finalizando o quiz e iniciando a pagina results

--- my-app/src/utils/index.jsx
+++ my-app/src/utils/index.jsx
@@ -5,3 +5,93 @@ export const redirect = (pages) => {
 export const clearing = () => {
 	localStorage.removeItem('members')
 }
+
+export const leader = (urlParams) => {
+	if(urlParams.leader === 'felipe') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const birthDateCell = (urlParams) => {
+	if(urlParams.leader === '2012') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const firstCellNumber = (urlParams) => {
+	if(urlParams.leader === '08') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const cellLocation = (urlParams) => {
+	if(urlParams.leader === 'cacula') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const dayCell = (urlParams) => {
+	if(urlParams.leader === 'sabado') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const leaderArrived = (urlParams) => {
+	if(urlParams.leader === '2009') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const shepherdName = (urlParams) => {
+	if(urlParams.leader === 'regis') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const communion = (urlParams) => {
+	if(urlParams.leader === 'salgados') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const disciplerName = (urlParams) => {
+	if(urlParams.leader === 'elaine') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
+
+export const howManyMembers = (urlParams) => {
+	if(urlParams.leader === '12') {
+		return(<img src='./icone-ok.png' alt='ícone de correto' />)
+	}
+	else {
+		return(<img src='./icone-x.png' alt='ícone de incorreto' />)
+	}
+}
