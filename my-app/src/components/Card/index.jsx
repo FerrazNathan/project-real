81a098a10253d543e408a3ae4b17f51a607214ea
Nathan Ferraz <nathanferraz@hotmail.com>
2022-06-27 17:48:16 -0300

    fix responsividade das paginas

--- my-app/src/components/Card/index.jsx
+++ my-app/src/components/Card/index.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-import * as S from './styles'
+import React from 'react';
+import * as S from './styles';
 
 export default function CardVideo({ video }) {
-	return (
-		<S.Container>
-			<S.Content>
-				<iframe
-					style={{
-						borderRadius: '16px',
-						boxShadow: '1px 5px 8px 5px rgba(0, 0, 0, 0.15)',
-					}}
-					width="100%"
-					height="100%"
-					src={video}
-					title="YouTube video player"
-					frameBorder="0"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowFullScreen
-				></iframe>
-			</S.Content>
-		</S.Container>
-	)
+  return (
+    <S.Container>
+      <S.Content>
+        <iframe
+          style={{
+            borderRadius: '16px',
+            boxShadow: '1px 5px 8px 5px rgba(0, 0, 0, 0.15)',
+          }}
+          width="100%"
+          height="100%"
+          src={video}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </S.Content>
+    </S.Container>
+  );
 }
